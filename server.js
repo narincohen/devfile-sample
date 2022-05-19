0240c453d4284f30dd24ed2775701017b81279e8
narincohen <62348102+narincohen@users.noreply.github.com>
2022-05-19 16:43:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.use(require('pino-http')({logger: pino}));
 
 app.get('/', (req, res) => {
   // Use req.log (a `pino` instance) to log JSON:
-  req.log.info({message: 'Hello EMET'});
-  res.send('Hello Emet!');
+  req.log.info({message: 'Hello inter-Serve!'});
+  res.send('Hello inter-Serve!');
 });
 
 app.get('*', (req, res) => {
